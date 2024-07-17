aa54881586519cd6791c98340aa32d7ebf6390f6
ridderp <ridderp05@gmail.com>
2024-07-17 12:28:33 -0400

    Added dodge logo and line break in the graph title

--- SinglePricePredictor.py
+++ SinglePricePredictor.py
@@ -49,7 +49,7 @@ print(str(totalWidth) + 'x' + str(totalHeight))
 solidColour = ColorClip(size=(totalWidth, totalHeight), color=(255, 255, 255), duration=resized_GraphClip.duration)
 
 # Title
-titleText = TextClip(carName + ' Price Predictions', font ="Arial-Bold", fontsize=50, color=carColor)
+titleText = TextClip(carName + '\n Price Predictions', font ="Arial-Bold", fontsize=50, color=carColor)
 titleText = titleText.set_duration(resized_GraphClip.duration)
 
 # Calculate the horizontal center position for the title text
--- interpolate.py
+++ interpolate.py
@@ -23,7 +23,7 @@ frames = 480
 
 car1ImageName = ''
 car2ImageName = ''
-car3ImageName = 'Jeep'
+car3ImageName = 'Dodge'
 
 # Not used here but still used in multiple files
 car1 = '' # Place Holder
