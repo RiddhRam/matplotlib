4339aed3c4ad65f66ac467dc46162ef641d48a9e
ridderp <ridderp05@gmail.com>
2024-07-20 11:54:32 -0400

    Added Volkswagen and Alpine Logo

--- interpolate.py
+++ interpolate.py
@@ -23,7 +23,7 @@ frames = 480
 
 car1ImageName = ''
 car2ImageName = ''
-car3ImageName = 'Alfa Romeo'
+car3ImageName = 'Alpine'
 
 # Not used here but still used in multiple files
 car1 = '' # Place Holder
